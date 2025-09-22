f725714d642d30584d2b9e880378a0856ae1336e
MichalK-KP <michalka@afk.no>
2025-09-22 14:28:34 +0200

    script

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let score = 0
+    let ScoreCount = document.getElementById("score")
+    ScoreCount.textContent = score
+    console.log(score)
+
+
+})
+
+
+function hjelp(){
+    console.log("abc")
+}
+
+function PilHoyre(){
+    
+}
